85bc1c09f4048f23efee715e5eb4927b9f9df876
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-11 22:09:28 +0100

    Add user class

--- Program.cs
+++ Program.cs
@@ -7,3 +7,13 @@ class Program
         Console.WriteLine("Hello World!");
     }
 }
+
+class User
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string FullName => $"{FirstName} {LastName}";
+}
